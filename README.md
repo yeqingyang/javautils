2eb98d948224d93777a77ae415c977b09a0db448
liupengzhan <liupengzhan@meituan.com>
2018-05-15 16:26:48 +0800

    添加版本号比较类

---
+++ README.md
@@ -0,0 +1,2 @@
+# 常用工具类
+- VersionUtil.java  版本号比较
---
+++ VersionUtil.java
@@ -0,0 +1,53 @@
+package com.sankuai.mall.promotion.capi.util;
+
+public class VersionUtil {
+    /**
+     * reference: https://stackoverflow.com/questions/6701948/efficient-way-to-compare-version-strings-in-java
+     *
+     * Compares two version strings.
+     *
+     * Use this instead of String.compareTo() for a non-lexicographical
+     * comparison that works for version strings. e.g. "1.10".compareTo("1.6").
+     *
+     * @note It does not work if "1.10" is supposed to be equal to "1.10.0".
+     *
+     * @param str1 a string of ordinal numbers separated by decimal points.
+     * @param str2 a string of ordinal numbers separated by decimal points.
+     * @return The result is a negative integer if str1 is _numerically_ less than str2.
+     *         The result is a positive integer if str1 is _numerically_ greater than str2.
+     *         The result is zero if the strings are _numerically_ equal.
+     */
+    public static int versionCompare(String str1, String str2) {
+        if (str1 == null && str2 == null) {
+            return 0;
+        } else if (str1 == null) {
+            return -1;
+        } else if (str2 == null) {
+            return 1;
+        }
+
+        String[] vals1 = str1.split("\\.");
+        String[] vals2 = str2.split("\\.");
+        int i = 0;
+        // set index to first non-equal ordinal or length of shortest version string
+        while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i])) {
+            i++;
+        }
+        // compare first non-equal ordinal number
+        if (i < vals1.length && i < vals2.length) {
+            int diff = atoi(vals1[i]).compareTo(atoi(vals2[i]));
+            return Integer.signum(diff);
+        }
+        // the strings are equal or one string is a substring of the other
+        // e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
+        return Integer.signum(vals1.length - vals2.length);
+    }
+
+    private static Integer atoi(String str) {
+        try {
+            return Integer.valueOf(str);
+        } catch (NumberFormatException e) {
+            return Integer.MIN_VALUE;
+        }
+    }
+}
